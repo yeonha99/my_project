4c785d257f57870bf7016daa5da17d6c065aab8f
yeonha99 <rhdqn1207@naver.com>
2022-02-21 18:47:29 +0900

    feat:함수 활용

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="name">{{greeting(name)}}</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup(){
+    const name='Kossie coder'
+
+    const greeting = (name) =>{
+      return 'Hello, '+name;
+    }
+
+      return{
+       name,
+       greeting,
+       };
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .name{
+    color:red;
+  }
 </style>
