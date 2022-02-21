4e3ea128c79a6f79f414af29dcdba02da9b3ac86
yeonha99 <rhdqn1207@naver.com>
2022-02-21 19:32:55 +0900

    feat:ref 와 reactive 사용

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,32 @@
   <div class="name">{{name}}</div>
   <button 
   class="btn btn-primary"
-  v-on:click="consoleLog"
+  v-on:click="updateName"
   >
   Click</button>
 </template>
 
 <script>
+import {ref} from 'vue';
+//숫자나 스트링 값 바꿀때 쓰는 것
+import {reactive} from 'vue';
+//오브젝트 값 바꿀때
+
 export default {
   setup(){
-    const name='Kossie coder'
+    const name=ref('Kossie coder1')
 
    // const greeting = (name) =>{
    //   return 'Hello, '+name;
   //  }
 
-    const consoleLog=()=>{
-      console.log('hello world');
+    const updateName=()=>{
+      name.value='Kossie Coder';
     };
 
       return{
        name,
-       consoleLog
+       updateName
        };
   }
 
